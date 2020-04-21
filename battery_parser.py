13d3598f91fe7a3d5e2e0188acb956d053af7ecf
BradleyFeng <bradly.feng@gmail.com>
2020-04-21 14:10:10 +0800

    [Battery-parser]Add parsing healthd/suspend keyword.

--- battery_parser.py
+++ battery_parser.py
@@ -1,16 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import sys 
+import sys
+import re
 
+#pattern
 a64_pattern_list=['charger->vbat', 'charger->ibat', 'charger->ocv', 'AxpOCV_percentage','AxpCoulumb_percentage', 'charger->new_capacity', 'charger->full_charging','charger->rest_vol']
 
-sc20_pattern_list=['calculated_soc', 'last_soc', 'bat_uv', 'last_ocv_uv','ocv_at_100','current_now']
+#sc20_pattern_list=['calculated_soc', 'last_soc', 'bat_uv', 'last_ocv_uv','ocv_at_100','current_now']
+#no support vbat
+sc20_pattern_list=['calculated_soc', 'last_soc', 'last_ocv_uv','current_now']
+#support vbat
+#sc20_pattern_list=['calculated_soc', 'last_soc', 'last_ocv_uv','current_now','bat_uv']
+
+suspend_healthd_list=['healthd: battery','PM: suspend entry','PM: suspend exit']
+
 time_vbat_list=[]
 time_ibat_list=[]
 time_ocv_list=[]
 time_ocvp_list=[]
 time_coulp_list=[]
 time_reprtp_list=[]
+
+#healthd_list
+time_healthd_list=[]
+time_suspend_list=[]
+soc_healthd_list=[]
+y_suspend_list=[]
 
 vbat_list=[] #bat_uv_list
 ibat_list=[]
@@ -33,23 +48,103 @@ percentage_ocv_range=[]
 
 start=False
 
-d={'a':"A64","s":"SC20","c":"Charging","d":"Discharging","r":"Range"}
-#for i in range(len(coul_percentage_list)):
-#    if 10 ==coul_percentage_list[i]:
-#        time_to_10=i
-#        break
+d={'a':"A64",'s':"SC20",'c':"Charging",'d':"Discharging",'r':"Range",'x':"suspend",'h':"healthd",'b':"heal/susd"}
 
 class battery_parser:
     def __init__(self):
         if len(sys.argv) < 4:
-            print 'Enter [a/s](platform:a64/sc20), [c/d](charge/discharge), [filename]'
+            print 'Enter [a/s](platform:a64/sc20), [c/d](charge/discharge), [filename] [h/x/b](use healthd/suspend/both keyword)'
             sys.exit(0)
         self.platform=sys.argv[1]
         self.mode=sys.argv[2]
         self.filename=sys.argv[3]
+        if (len(sys.argv)==5):
+            self.keyword=sys.argv[4]
+        else:
+            self.keyword="None"
+
         self.fd=open(self.filename)
         self.time_to_100=0
-        print("Platform = " + d[self.platform] + "\nDiagram = " + d[self.mode] + "\nFile = " + self.filename)
+        self.basetime=0
+        self.start_date=""
+        self.suspend_start=False
+        print("Platform = " + d[self.platform] + "\nDiagram = " + d[self.mode] + "\nFile = " + self.filename + "\nparsing Keyword = " + self.keyword)
+
+    def healthd(self,line):
+        time_healthd_list.append(self.gettime(line)-self.basetime)
+        match=re.search(r"l=\d* v=\d* t=\d*.\d h=\d st=\d c=\d*",line)
+        if(match):
+            healthd_str = match.group()
+            soc_healthd_list.append(int((healthd_str.split(" ")[0]).split("=")[1]))
+            vbat_list.append(int((healthd_str.split(" ")[1]).split("=")[1]))
+
+    def suspend(self,line):
+        if(self.suspend_start==False and line.find('PM: suspend exit')==0):
+            return
+        else:
+            self.suspend_start = True
+        if(line.find('PM: suspend entry')==0):
+            time_suspend_list.append(self.gettime(line)-self.basetime)
+            time_suspend_list.append(self.gettime(line)-self.basetime)
+            y_suspend_list.append(5)
+            y_suspend_list.append(100)
+        if(line.find('PM: suspend exit')==0):
+            time_suspend_list.append(self.gettime(line)-self.basetime)
+            time_suspend_list.append(self.gettime(line)-self.basetime)
+            y_suspend_list.append(100)
+            y_suspend_list.append(5)
+
+    def gettime(self,line):
+        match=re.search(r"\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{9}",line)
+        if(match):
+            time_str=match.group()
+            time_str_list=time_str.split(" ")
+            self.start_date=time_str_list[0]
+            basetime=time_str_list[1].split(":")
+            return round((int(basetime[0])*3600)+(int(basetime[1])*60)+(float(basetime[2])))
+
+    def suspend_healthd_parser(self):
+        for line in self.fd:
+            time_start=line.find('[')
+            time_end=line.find('.')
+            symbol_end=line.find(']')
+            line=line[symbol_end+2:] #Remove space
+            res=[i for i in suspend_healthd_list if i in line]
+            if len(res)==0:
+                continue
+            if(self.basetime == 0):
+                self.basetime=self.gettime(line)
+                print self.basetime
+            if(line.find('healthd: battery')==0):
+                self.healthd(line)
+            if(line.find('PM: suspend')==0):
+                self.suspend(line)
+        #print time_suspend_list
+        #print y_suspend_list
+        #print soc_healthd_list
+    
+    def healthd_suspend_plot(self):
+        xtime = time_suspend_list 
+        plt.ylim(0,105)
+        new_ticks = np.linspace(0, 100, 21)
+        y = y_suspend_list
+        plt.plot(xtime,y,'r',label="suspend")
+
+        plt.yticks(new_ticks)
+        plt.scatter(xtime, y, color='g') # link point to point
+        plt.legend(loc='best')
+       
+        xh=time_healthd_list
+        yh=soc_healthd_list
+        plt.grid()
+        plt.plot(xh,yh,'y', label="soc")
+
+        ax2=plt.twinx()
+        y_vbat = vbat_list
+        ax2.set_ylabel("Voltage")
+        ax2.plot(xh,y_vbat, color="b", label="Vbat Voltage")
+        ax2.legend(loc='lower right')
+        #plt.scatter(xh, yh, color='b') # link point to point
 
     def sc20_parser(self):
         result_list=[]
@@ -69,12 +164,13 @@ class battery_parser:
                 if len(res)==0:
                     continue
                 if a[0] == sc20_pattern_list[0]:
-                    if len(result_list) == 5:
+                    if len(result_list) >= len(sc20_pattern_list):
                         calsoc_list.append(result_list[0])
                         reprt_percentage_list.append(result_list[1])
-                        vbat_list.append(result_list[2])
-                        ocv_list.append(result_list[3])
-                        ibat_list.append(result_list[4])
+                        ocv_list.append(result_list[2])
+                        ibat_list.append(result_list[3])
+                        if len(sc20_pattern_list) == 5:  
+                            vbat_list.append(result_list[4])
                     result_list = []
                     #start=True
                     h_flag=False
@@ -92,8 +188,11 @@ class battery_parser:
                 elif a[0] == sc20_pattern_list[2]:
                     result_list.append(int(a[1]))
                 elif a[0] == sc20_pattern_list[3]:
-                    result_list.append(int(a[1]))
-                elif a[0] == sc20_pattern_list[5]:
+                    if int(a[1]) <= 0:
+                        result_list.append(abs(int(a[1])))
+                    else:
+                        result_list.append(0)
+                elif a[0] == sc20_pattern_list[4]:
                     result_list.append(int(a[1]))
                 else:
                     continue
@@ -173,18 +272,19 @@ class battery_parser:
                         #print (ibat_list[i],i)
 
         #vbat plot
-        batp_100.sort()
-        xbat = np.arange(0,len(batp_100))
-        ybat = batp_100
-        plt.ylabel("mv")
-        plt.plot(xbat,ybat,'b',label="vbat range")
-        plt.annotate('(bat min=%smv)' % (batp_100[0]), xy=(0,batp_100[0]), xycoords='data', xytext=(+30, -30),
-                     textcoords='offset points', fontsize=16,
-                     arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=.2"))
-        plt.annotate('(bat max=%smv)' % (batp_100[len(batp_100)-1]),
-                     xy=(len(batp_100),batp_100[len(batp_100)-1]), xycoords='data', xytext=(+30, -30),
-                     textcoords='offset points', fontsize=16,
-                     arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=.2"))
+        if(len(batp_100) != 0):
+            batp_100.sort()
+            xbat = np.arange(0,len(batp_100))
+            ybat = batp_100
+            plt.ylabel("mv")
+            plt.plot(xbat,ybat,'b',label="vbat range")
+            plt.annotate('(bat min=%smv)' % (batp_100[0]), xy=(0,batp_100[0]), xycoords='data', xytext=(+30, -30),
+                         textcoords='offset points', fontsize=16,
+                         arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=.2"))
+            plt.annotate('(bat max=%smv)' % (batp_100[len(batp_100)-1]),
+                         xy=(len(batp_100),batp_100[len(batp_100)-1]), xycoords='data', xytext=(+30, -30),
+                         textcoords='offset points', fontsize=16,
+                         arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=.2"))
 
         # ocv plot
         ocv_100_len = len(ocvp_100)
@@ -262,19 +362,21 @@ class battery_parser:
         y_ocv = ocv_list
         plt.subplot(212)
         plt.ylabel("voltage")
-        plt.plot(x_bat,y_bat,label="Bat uv")
+        if(len(vbat_list)!=0):
+            plt.plot(x_bat,y_bat,label="Bat uv")
         plt.plot(x_ocv,y_ocv,label="OCV uv")
         plt.grid()
         plt.legend(loc='best')
 
-        plt.annotate('(%ss, vbat=%s uV)' % (self.time_to_100,vbat_list[self.time_to_100]), xy=(self.time_to_100,vbat_list[self.time_to_100]),
-                     xycoords='data', xytext=(+30, -30),
-                     textcoords='offset points', fontsize=16,
-                     arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=.2"))
-        plt.annotate('(%ss, ocv=%s uV)' % (self.time_to_100,ocv_list[self.time_to_100]), xy=(self.time_to_100,ocv_list[self.time_to_100]),
-                     xycoords='data', xytext=(+80, -80),
-                     textcoords='offset points', fontsize=16,
-                     arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=.2"))
+        if self.time_to_100 != 0:
+            plt.annotate('(%ss, vbat=%s uV)' % (self.time_to_100,vbat_list[self.time_to_100]), xy=(self.time_to_100,vbat_list[self.time_to_100]),
+                         xycoords='data', xytext=(+30, -30),
+                         textcoords='offset points', fontsize=16,
+                         arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=.2"))
+            plt.annotate('(%ss, ocv=%s uV)' % (self.time_to_100,ocv_list[self.time_to_100]), xy=(self.time_to_100,ocv_list[self.time_to_100]),
+                         xycoords='data', xytext=(+80, -80),
+                         textcoords='offset points', fontsize=16,
+                         arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=.2"))
         for i in range(len(ibat_start_list)):
             plt.annotate('(%ss, vbat=%smV)' % (ibat_start_list[i],vbat_list[ibat_start_list[i]]),
                          xy=(ibat_start_list[i],vbat_list[ibat_start_list[i]]), xycoords='data', xytext=(-50, +20),
@@ -285,6 +387,14 @@ class battery_parser:
                          xy=(ibat_low_list[i],ocv_list[ibat_low_list[i]]), xycoords='data', xytext=(+30, -100),
                          textcoords='offset points', fontsize=16,
                          arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=.2"))
+
+        ax2=plt.twinx()
+        #Charging Current plot
+        x_ibat = np.arange(0,len(ibat_list))
+        y_ibat = ibat_list
+        ax2.set_ylabel("Current")
+        ax2.plot(x_ibat,y_ibat, color="r", label="charging current")
+        ax2.legend(loc='lower right')
 
     def a64_charge_plot(self):
         plt.subplot(211)
@@ -376,7 +486,7 @@ class battery_parser:
         plt.ylabel("capacity")
         plt.grid()
         plt.plot(x_calsoc,y_calsoc,'b',label="Calculated Soc",)
-        plt.plot(x_realsoc,y_realsoc, 'r',label="Displayed Soc")
+        plt.plot(x_realsoc,y_realsoc, 'r:',label="Displayed Soc")
         plt.legend(loc='best')
         if (self.time_to_100 != 0):
             plt.annotate('(%ss, %s%%, ocv=%smV)' % (self.time_to_100,100,ocv_list[self.time_to_100]), xy=(self.time_to_100,100),
@@ -421,14 +531,19 @@ class battery_parser:
 
 if __name__ == '__main__':
     bat=battery_parser()
-    if bat.platform == 'a':
+    if bat.keyword == 'h' or bat.keyword == 'x' or bat.keyword == 'b':
+        bat.suspend_healthd_parser()
+    elif bat.platform == 'a':
         bat.a64_parser()
     elif bat.platform == 's':
         bat.sc20_parser()
     else:
         print 'assign platform (a or s) => (a64 or sc20)'
 
-    if bat.mode == 'c':
+    if bat.keyword == 'h' or bat.keyword == 'x' or bat.keyword == 'b':
+        bat.healthd_suspend_plot()
+        plt.show()
+    elif bat.mode == 'c':
         bat.charge_plot()
         plt.show()
     elif bat.mode == 'd':
